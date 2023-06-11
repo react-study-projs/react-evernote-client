328367388a5fd86ae3c5642aa4e01cb4af7c7162
jaychenfe <chenjiehwy@163.com>
2023-06-11 11:19:44 +0800

    增加全局样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/router'
-import './index.css'
+import '@/common/styles/frame.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<RouterProvider router={router} />)
