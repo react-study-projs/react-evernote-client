7956191f8cda6fcc20c73ebe0f2ba171a09b8d32
jaychenfe <chenjiehwy@163.com>
2023-06-12 13:13:51 +0800

    封装axios

---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import axios, { InternalAxiosRequestConfig, AxiosRequestConfig, AxiosResponse } from 'axios'
+import localforage from 'localforage'
+import { globalConfig } from '@/globalConfig'
+import { message } from 'antd'
+import { router } from '@/router'
+
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+axios.interceptors.request.use(
+    async (config: InternalAxiosRequestConfig) => {
+        const token = (await localforage.getItem(globalConfig.STROGE_KEY_TOKEN)) || 'token'
+
+        config.headers.Authorization = 'Bearer ' + token
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+// 统一的错误处理函数
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const handleRequestError = (error: any) => {
+    console.error('请求错误:', error)
+    message.error('网络异常')
+    throw new Error('网络异常')
+}
+
+export default function sendRequest<T>(
+    url: string,
+    type: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any = {}
+): Promise<T> {
+    return new Promise((resolve, reject) => {
+        const options: AxiosRequestConfig = {
+            url,
+            method: type,
+            validateStatus(status) {
+                return (status >= 200 && status < 300) || status === 400 || status === 401
+            },
+        }
+
+        if (type.toLowerCase() === 'get') {
+            options.params = data
+        } else {
+            options.data = data
+        }
+
+        axios(options)
+            .then((res: AxiosResponse<T>) => {
+                if (res.status >= 200 && res.status < 400) {
+                    resolve(res.data)
+                } else {
+                    reject(res.data)
+                }
+
+                if (res.status === 401) {
+                    message.error('请重新登陆')
+                    router.navigate('/login')
+                }
+            })
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .catch((error: any) => {
+                handleRequestError(error)
+            })
+    })
+}
